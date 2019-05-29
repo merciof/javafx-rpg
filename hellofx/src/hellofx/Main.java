c4c06d9a18b70f767defccda0956bd14f413b7e0
merciof <mercio.andrade@ufrpe.br>
2019-05-29 16:26:26 -0300

    Definição de atributos da VBox e adição de makeMove() e MakeAiMove()

--- hellofx/src/hellofx/Main.java
+++ hellofx/src/hellofx/Main.java
@@ -1,39 +1,38 @@
  package hellofx;
 
+import java.util.HashMap;
+import java.util.Map;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    private void makeMove(Action action) {
+    
+    private TextArea output = new TextArea();
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+         stage.setScene(new Scene(createContent()));
+         stage.show();
+    }
+    
+    private Parent createContent() {
         
-    }
-
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root));
-//        primaryStage.show();
-//    }
-
-
-    private enum Action{
-        ATTACK, CHARGE, BLOCK;
-    }
-    
-    private enum ActionResult{
-        WIN, LOSE, DRAW;
-    }
-    
-    private Parent createContent(){
         VBox root = new VBox(10);
+        
+        root.setPrefSize(800, 600);
+        
+        root.setPadding(new Insets(15));
+        
+        output.setPrefHeight(540);
+        output.setFont(Font.font(25));
         
         Button btnAttack = new Button("Attack");
         btnAttack.setOnAction(e -> makeMove(Action.ATTACK));
@@ -44,17 +43,33 @@ public class Main extends Application {
         Button btnBlock = new Button("BLOCK");
         btnBlock.setOnAction(e -> makeMove(Action.BLOCK));
         
+        root.getChildren().addAll(btnAttack, btnCharge, btnBlock, output);
+        
         return root;
     }
     
-    @Override
-    public void start(Stage stage) throws Exception {
-         stage.setScene(new Scene(createContent()));
-         stage.show();
+    
+    private void makeMove(Action userAction) {
+        Action aiAction = makeAiMove();
+        
+        ActionResult result = userAction.checkAgainst(aiAction);
+        
+        if(result == ActionResult.DRAW) {
+            output.appendText("DRAW\n");
+        } else if(result == ActionResult.WIN) {
+            output.appendText("WIN\n");
+        } else { //LOSE
+            output.appendText("LOSE\n");
+        }
+        
     }
+    
+    private Action makeAiMove() {
+        return Action.values()[(int) (Math.random() * Action.values().length)];
+    }
+    
 
     public static void main(String[] args) {
         launch(args);
-     
     }
 }
